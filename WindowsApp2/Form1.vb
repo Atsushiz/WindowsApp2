fe6c7bfce55a0ec3ce72e0ec2a935644d8e328b6
Atsushi Nishiyama <cp527242@kc4.so-net.ne.jp>
2018-12-08 20:41:57 +0900

    20181208_2041

--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -5,5 +5,7 @@
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Label1.Text = DateString + " " + TimeString
+        Label1.Text = DateString + " " + TimeString
+
     End Sub
 End Class
