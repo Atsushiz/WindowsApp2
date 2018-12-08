3484db1d8c485c2654496f80875a54cd1b262b43
Atsushi Nishiyama <cp527242@kc4.so-net.ne.jp>
2018-12-08 17:38:15 +0900

    20181208

--- WindowsApp2/Form1.vb
+++ WindowsApp2/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label1.Text = "テストメッセージ"
+    End Sub
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = DateString + " " + TimeString
+    End Sub
 End Class
